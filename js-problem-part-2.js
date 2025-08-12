3ef8a24c6b07cfe97e54224a51f3ed6a3790dc8d
tumpa <tumpadas3494@gmail.com>
2025-08-12 21:40:07 +0600

    all qs done

--- js-problem-part-2.js
+++ js-problem-part-2.js
@@ -94,14 +94,14 @@ If you really want to keep "tk" in the data but still calculate, you’d have to
 //     ];
 
 
-    const phones = [
-        { model: "PhoneA", brand: "Iphone", price: 95000 },
-        { model: "PhoneB", brand: "Samsung", price: 40000 },
-        { model: "PhoneC", brand: "Oppo", price: 26000 },
-        { model: "PhoneD", brand: "Nokia", price: 35000 },
-        { model: "PhoneE", brand: "Iphone", price: 105000 },
-        { model: "PhoneF", brand: "HTC", price: 48000 },
-    ];
+//     const phones = [
+//         { model: "PhoneA", brand: "Iphone", price: 95000 },
+//         { model: "PhoneB", brand: "Samsung", price: 40000 },
+//         { model: "PhoneC", brand: "Oppo", price: 26000 },
+//         { model: "PhoneD", brand: "Nokia", price: 35000 },
+//         { model: "PhoneE", brand: "Iphone", price: 105000 },
+//         { model: "PhoneF", brand: "HTC", price: 48000 },
+//     ];
 
 //     function findAveragePhonePrice(phones){
 //           let totalPrice = 0;
@@ -124,6 +124,35 @@ If you really want to keep "tk" in the data but still calculate, you’d have to
       
 // }
 
+// For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
 
+//  const employees = [
+//             { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+//             { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+//             { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+//             { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+//         ];
+
+
+//  const employees = [
+//             { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+//             { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+//             { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+//             { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+//         ];
+        
+
+// for (let i = 0; i < employees.length; i++) {
+//       let experience = employees[i].experience;
+//    let starting = employees[i].starting;
+//    let increment = employees[i].increment;
+
+//    let currentSalary = starting + (experience * increment);
+      
+//       let name = employees[i].name;
+
+//       console.log(name + ": " + currentSalary);
+// }
+      
 
 

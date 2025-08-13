f74187938429a34334e2b9cd12c15130ba996a49
tumpa <tumpadas3494@gmail.com>
2025-08-13 12:10:38 +0600

    task 1 completed

---
+++ conceptual_session_qs.js
@@ -0,0 +1,30 @@
+// Task 1 :
+// Create a Function sum which take 2 value and return the sum of 2 number
+// Create a Function multiply which take 2 value and return the multiply of 2 number
+// Suppose you have 2 numbers a , b print the sum of ( a + b) and ( a x b ) using sum and
+// multiply function.
+
+// function sum(input1, input2) {
+//       let result = input1 + input2;
+//       // console.log(result);
+//       return result;
+// }
+// sum(2, 3);
+
+// function multiply(input1, input2) {
+//      let result = input1 * input2;
+//       //  console.log(multiply);
+//       return result;
+// }
+// multiply(2, 6);
+
+// let a = 2;
+// let b = 3;
+
+// let result = sum(a, b) + multiply(a, b);
+// console.log(result);
+
+// Task 2:
+// Create a function sumOfArray which takes an array and returns the sum of that array. If the
+// input is not an array and if any data not a number found in the array , it will return ‘Invalid’
+

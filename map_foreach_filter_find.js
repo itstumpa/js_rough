a498eef70d8390f5368816e54ee872ed04b7fb34
tumpa <tumpadas3494@gmail.com>
2025-09-03 23:15:45 +0600

    .

--- map_foreach_filter_find.js
+++ map_foreach_filter_find.js
@@ -1,13 +1,13 @@
 const students = [
-      {id:1, name: "abul", marks: 50},
-      {id:2, name: "kabul", marks: 80},
-      {id:3, name: "mabul", marks: 90},
-      {id:4, name: "cabul", marks: 10},
+      { id: 1, name: "abul", marks: 50 },
+      { id: 2, name: "kabul", marks: 80 },
+      { id: 3, name: "mabul", marks: 90 },
+      { id: 4, name: "cabul", marks: 10 },
 ]
 
 const names = students.map(student => student.marks * 0.75)
-const goodStudents = students.filter (student => student.marks > 80)
-const goodStudent = students.find (student => student.marks > 80)
+const goodStudents = students.filter(student => student.marks > 80)
+const goodStudent = students.find(student => student.marks > 80)
 
 
 
